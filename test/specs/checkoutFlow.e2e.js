854617c14ee4b854c0ce5294e549c4cd5d3dac79
Elshauna Dixon <shaunadixon1@gmail.com>
2022-11-18 15:04:30 -0500

    Updating the test scripts for execution

--- test/specs/checkoutFlow.e2e.js
+++ test/specs/checkoutFlow.e2e.js
@@ -3,7 +3,7 @@ const checkout = require("../pageobjects/checkout");
 const loginPage = require("../pageobjects/login.page");
 
 
-describe.only('Checkout Flow', ()=>{
+describe.skip('Checkout Flow', ()=>{
    //Should login before each it block.
    beforeEach(async()=> {
       await loginPage.open();
@@ -15,7 +15,7 @@ describe.only('Checkout Flow', ()=>{
       //Purchasing the product
       await checkout.purchaseProduct("Kings Place","Shop 55","East 55th Street","China Town","New Orleans","Florida","12345-6789","1234567890");
    });
-   it('should verify purchase in order history',async ()=>{
+   xit('should verify purchase in order history',async ()=>{
       await checkout.verifyPurchase(); //elements can be found in the checkout.js file
    });
    
